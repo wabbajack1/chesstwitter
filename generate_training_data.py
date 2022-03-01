ef4eb0ec0f3ce47b8965d4a3a9b5b501b939eaed
wabbajack <kidim4444@gmail.com>
2022-03-01 12:49:56 +0100

    seriliaze data

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,43 +1,54 @@
 #!/usr/bin/env python3
-import os
+
+import os, sys
+
+sys.path.append("lib/")
 import chess.pgn
 import numpy as np
+from state import State
 
-
-"""
+def make_dataset():
+    """
     Before we can feed data in the net, we must first:
     1. parse chess games
     2. serialize the game in the format for the net (each move)
-    3. train the net with the serialized data
-"""
+    3. train the net with the serialized data (find a linear combination of all the selected features)
 
-def make_dataset():
+    Returns:
+        np.array: train, labels
+    """
     x, y = [], [] # x train, y label
     values = {'1/2-1/2':0, '0-1':-1, '1-0':1} # in centipawns, maxplayer is white
     gm = 0 # number of game
 
     for f in os.listdir("data"):
-        ff = open(os.path.join("data", f), "r")
-        while True:
-            game = chess.pgn.read_game(ff)
-            if game is None:
-                break
-
-            val = game.headers['Result']
-            if val not in values:
-                continue
         
-            board = game.board()
-            value = values[val]
-            for i, move in enumerate(game.mainline_moves()):
-                board.push(move)
-                #print(board, "\n")
-            y.append(value)
-            gm += 1
-            print("Game {:d}".format(gm))
-    return 
+        with open(os.path.join("data", f), "r") as ff:
+            while True:
+
+                game = chess.pgn.read_game(ff)
+                
+                if game is None:
+                    break
+
+                val = game.headers['Result']
+                if val not in values:
+                    continue
+            
+                board = game.board()
+                value = values[val]
+                for i, move in enumerate(game.mainline_moves()):
+                    board.push(move)
+                    data = State(board).serialize()
+                    x.append(data)
+                    y.append(value)
+                    #print(board, "\n")
+                gm += 1
+                print("Game {:d}, got {} positions".format(gm, len(x)))
+    return np.array(x), np.array(y)
 
 
 if __name__ == "__main__":
-    make_dataset()
+    x, y = make_dataset()
+    np.savez("chess_nn_data.npz", x, y)
 
--- state.py
+++ state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import sys
+
+sys.path.append("lib/python3.9/site-packages")
+
 import chess
-from torch import nn
 import numpy as np
+
 
 
 class State(object):
@@ -12,18 +15,58 @@ class State(object):
         else:
             self.board = board
 
-        self.values = {"p": 1, "b":2, "n": 3, "r": 4, "q":5, "k":6, 
-                      "P": 7, "B":8, "N": 9, "R": 10, "Q":11, "K":12} # we have to encode the states (here look at FEN states)
+        self.values = {"p": 2, "b":4, "n": 8, "r": 16, "q":32, "k":64, 
+                      "P": 128, "B":256, "N": 512, "R": 1024, "Q":2048, "K":4096} # we have to encode the states (here look at FEN states)
 
     def current_state(self):
         return (self.board.board_fen(), self.board.ep_square, self.board.castling_rights, self.board.turn)
 
     def serialize(self):
+        """
+        Serialize the data to a representation, that we can feed out Model (here an CNN). So we Bitboard the states.
+        --> https://arxiv.org/pdf/1711.09667.pdf deepchess ideas. CNN is incorporated into a new
+        form of alpha-beta search.
+        """
         assert self.board.is_valid()
-        state = np.zeros((64,))
-
         
+        # additional states for represent the board
+        bit_board = np.zeros((64,), dtype=np.int64)
+        state = np.zeros((16, 8, 8), dtype=np.int64)
+
+        for i in range(len(bit_board)):
+            p_symbol = self.board.piece_at(i)
+            if p_symbol is not None:
+                bit_board[i] = self.values[p_symbol.symbol()] # insert the values
+
+        if self.board.has_kingside_castling_rights(chess.WHITE):
+            cas = np.zeros((8,8))
+            cas[0,0] = 1
+            state[0] = cas
+        if self.board.has_queenside_castling_rights(chess.WHITE):
+            cas = np.zeros((8,8))
+            cas[0,7] = 1
+            state[1] = cas
+        if self.board.has_kingside_castling_rights(chess.BLACK):
+            cas = np.zeros((8,8))
+            cas[-1,0] = 1
+            state[2] = cas
+        if self.board.has_queenside_castling_rights(chess.BLACK):
+            cas = np.zeros((8,8))
+            cas[-1,7] = 1
+            state[3] = cas
+
+        bit_board = bit_board.reshape((8,8))
+
+        bits = 1
+        for i in range(4, state.shape[0]):
+            state[i] = (bit_board >> bits)&1
+            bits += 1
+            #print(i, bits)
+
+        return state
 
 
 if __name__ == "__main__":
-    pass
+    board = chess.Board()
+    s = State(board)
+    s.serialize()
